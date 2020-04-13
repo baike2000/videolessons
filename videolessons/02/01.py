0f5bafc9180d0f478f38619dcdf62a3c073c676c
artem <baike2000@gmail.com>
2020-04-14 00:33:37 +0600

    2 урок

--- videolessons/02/01.py
+++ videolessons/02/01.py
@@ -11,14 +11,16 @@ for i in range(N):
     print() # переход на следующую  строку
 
 #начальные значения максимального и минимального элементов
-maxi, maxj = 0, 0 
-mini, minj = 0, 0
 for i in range(N): 
     for j in range(N):
-        if A[i][j] > A[maxi][maxj]:
-            maxi, maxj = i, j
-        if A[i][j] < A[mini][minj]:
-            mini, minj = i, j
-            
-print("Максимальный элемент A[{0},{1}]={2}".format(maxi,maxj,A[maxi][maxj]))
-print("Минимальный элемент A[{0},{1}]={2}".format(mini,minj,A[mini][minj]))
+        if (j > i):
+            A[i][j] = 0
+        else:
+            A[i][j] -= 1
+
+print("Результат:")
+for i in range(N): 
+    for j in range(N): 
+        print ("{:4d}".format(A[i][j]), end = "")
+    print() # переход на следующую  строку
+           
